d5aecfa16cf0b26450619f250a3268a0c58b8957
Christian Gulliksen <christiangulliksen@finkoslo.no>
2018-12-06 13:58:49 +0100

    notification and audio

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,16 @@
-setTimeout(function(){ alert("Hello"); }, 2000);
+//setTimeout(function(){ alert("Hello"); }, 2000);
 
 window.onscroll = function () {
     window.scrollTo(0,0);
-    }
+}
+
+function pushNotification() {
+    let notification = document.getElementById('notification');
+    notification.classList.add('active');
+    let audio = document.getElementById('audio');
+    audio.play();
+
+}
+setTimeout( function() {
+    pushNotification();
+}, 2000)
