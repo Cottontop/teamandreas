318984eb82036e61ad9e45a3fff6ef2375cfe2d3
Christian Gulliksen <christiangulliksen@finkoslo.no>
2018-12-06 16:10:47 +0100

    Parsed JSON

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const campaigns = require("../lib/campaigns").campaigns;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	const campaignJSON = campaigns[Math.floor(Math.random() * Math.floor(campaigns.length))];
+	const campaignJSON = JSON.stringify(campaigns[Math.floor(Math.random() * Math.floor(campaigns.length))]);
 	res.render('index',{campaign: campaignJSON});
 });
 
