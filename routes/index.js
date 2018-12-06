7441e811929acafe58ac6c3c8ece1ff9e685a574
Syver Bolstad <sb@itverket.no>
2018-12-06 16:12:50 +0100

    added noJson

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,8 @@ const campaigns = require("../lib/campaigns").campaigns;
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	const campaignJSON = JSON.stringify(campaigns[Math.floor(Math.random() * Math.floor(campaigns.length))]);
-	res.render('index',{campaign: campaignJSON});
+	const campaign = campaigns[Math.floor(Math.random() * Math.floor(campaigns.length))];
+	res.render('index',{campaign: campaignJSON, noJson: campaign});
 });
 
 module.exports = router;
